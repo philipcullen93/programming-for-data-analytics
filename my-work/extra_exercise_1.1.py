7c82ee2cbf4a6621a5ebece3a189813d3fa10e39
philipcullen93 <philipcullen93@gmail.com>
2025-09-30 17:21:10 +0000

    Pulled down data from .csv file and created code to make a graph displaying Birth Rates by Year

--- my-work/extra_exercise_1.1.py
+++ my-work/extra_exercise_1.1.py
@@ -1,2 +1,17 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+
+# creates a dataframe using the data from the .csv file
+df = pd.read_csv("https://raw.githubusercontent.com/andrewbeattycourseware/PFDA-courseware/refs/heads/main/code/data/projectedbirths-cso.csv")
+
+# displays the dataframe, I used this to see if the correct data was pulled 
+print(df.head())
+
+# create a simple plot using the data provided
+plt.plot(df['Year'], df['VALUE'])
+plt.xlabel('Year')
+plt.ylabel('Birth Rate')
+plt.title('Projected Birth rates by Year')
+plt.show()
+
+plt.savefig("my-work/projected_births.png")
